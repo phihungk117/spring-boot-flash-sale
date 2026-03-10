d4902e0b8a6458f69711dcd3de80d50d29b69236
hidas2004 <phihungbp1174@gmail.com>
2026-03-10 18:41:15 +0700

    init: setup Spring Boot project with dependencies

---
+++ spring-boot-flash-sale/src/main/java/com/example/spring_boot_flash_sale/SpringBootFlashSaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_flash_sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlashSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFlashSaleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-flash-sale/src/test/java/com/example/spring_boot_flash_sale/SpringBootFlashSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_flash_sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFlashSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
